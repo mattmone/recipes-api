7946199058b13327d22862e8af8a441154eca537
Matthew Mone <mmone@vernier.com>
2020-06-08 13:18:08 -0700

    move from puppetter to cheerio

--- src/http/post-recipe/index.js
+++ src/http/post-recipe/index.js
@@ -1,39 +1,51 @@
 const data = require('@begin/data');
-const begin = require('@architect/functions'); // Reads & writes session data
-const puppeteer = require('puppeteer')
+const https = require('https');
+const cheerio = require('cheerio');
+
+function getHtml(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let html = '';
+      response.on('data', (chunk) => {
+        html += chunk
+      });
+      response.on('end', () => {
+        resolve(html);
+      })
+    }).on('error', (error) => {
+      reject(error);
+    })
+  })
+}
 
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 exports.handler = async function http (request) {
-  const session = await begin.http.session.read(request);
   const bodyBuffer = Buffer.from(request.body, 'base64');
   const url = bodyBuffer.toString('utf-8');
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
+  const html = await getHtml(url);
+  const $ = cheerio.load(html)
+
+  const extractContent = (nodes) => {
+    return Array.from(nodes.map((i, n) => $(n).text().trim())).filter((n) => n)
+  }
+  const buildRecipeCard = (titleSelector, ingredientSelector, directionSelector) => {
+    return {
+      title: $(titleSelector).text(),
+      ingredients: extractContent($(ingredientSelector)),
+      directions: extractContent($(directionSelector))
+    }
+  }
+  const pullRecipeCard = () => {
+    if($('.easyrecipe').is('.easyrecipe')) return buildRecipeCard('.ERSName', '.ingredient', '.instruction');
+    if(url.match(/allrecipes.com/)) return buildRecipeCard("#recipe-main-content", ".recipe-ingred_txt", '.recipe-directions__list--item')
+    if(url.match(/pamperedchef.com/)) return buildRecipeCard("#recipeName", "#rpIngredients li", '#rpDirections li')
+    if($('.wprm-recipe-container').is('.wprm-recipe-container')) return buildRecipeCard('.wprm-recipe-name', '.wprm-recipe-ingredient', '.wprm-recipe-instruction')
+    return {"message": "Could not extract recipe."};
+  }
+  const recipe = JSON.stringify(pullRecipeCard());
   
-  const recipe = await page.evaluate(() => {
-    function extractContent(nodes) {
-      return Array.from(nodes).map(n => n.textContent.trim()).filter(n => n)
-    }
-    function buildRecipeCard(titleSelector, ingredientSelector, directionSelector) {
-      return {
-        title: document.querySelector(titleSelector).textContent,
-        ingredients: extractContent(document.querySelectorAll(ingredientSelector)),
-        directions: extractContent(document.querySelectorAll(directionSelector))
-      }
-    }
-    function pullRecipeCard() {
-      if(document.querySelector('.easyrecipe')) return buildRecipeCard('.ERSName', '.ingredient', '.instruction');
-      if(location.host.match(/allrecipes.com/)) return buildRecipeCard("#recipe-main-content", ".recipe-ingred_txt", '.recipe-directions__list--item')
-      if(location.host.match(/pamperedchef.com/)) return buildRecipeCard("#recipeName", "#rpIngredients li", '#rpDirections li')
-      if(document.querySelector('.wprm-recipe-container')) return buildRecipeCard('.wprm-recipe-name', '.wprm-recipe-ingredient', '.wprm-recipe-instruction')
-      return {"message": "Could not extract recipe."};
-    }
-    return JSON.stringify(pullRecipeCard());
-  });
   data.set({table: "recipes", recipe})
-  await browser.close();
   return {
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
