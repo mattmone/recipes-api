953ba65fbd807bf609c9254b519ab777d86620c8
Matthew Mone <mmone@vernier.com>
2020-06-09 16:55:25 -0700

    stringify the body

--- src/http/post-recipe/index.js
+++ src/http/post-recipe/index.js
@@ -51,6 +51,6 @@ exports.handler = async function http (request) {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
       'content-type': 'application/json; charset=utf8'
     },
-    body: recipe
+    body: JSON.stringify(recipe)
   }
 }
