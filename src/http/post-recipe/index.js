af33ff52490564fded561a8b2bdf25590595d420
Matthew Mone <mmone@vernier.com>
2020-06-20 16:56:32 -0700

    return to json return

--- src/http/post-recipe/index.js
+++ src/http/post-recipe/index.js
@@ -51,9 +51,9 @@ exports.handler = async function http (request) {
   return {
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8'
+      'content-type': 'application/json; charset=utf8'
     },
-    body: html
+    body: JSON.stringify(recipe)
   }
 }
 
