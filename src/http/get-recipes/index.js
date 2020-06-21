ab5b2cd740fa913b377958d572f2702fd59fd4c3
Matthew Mone <mmone@vernier.com>
2020-06-20 17:07:20 -0700

    filter invalid recipes

--- src/http/get-recipes/index.js
+++ src/http/get-recipes/index.js
@@ -3,6 +3,7 @@ let data = require('@begin/data')
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 exports.handler = async function http (req) {
   const recipes = await data.get({table: 'recipes'})
+  recipes.filter(recipe => recipe.title)
   return {
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
