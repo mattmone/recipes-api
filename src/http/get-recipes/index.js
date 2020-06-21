43deeecd04caa1e970721e2864b738fdb193362b
Matthew Mone <mmone@vernier.com>
2020-06-20 17:15:05 -0700

    debugging?

--- src/http/get-recipes/index.js
+++ src/http/get-recipes/index.js
@@ -2,7 +2,7 @@ let data = require('@begin/data')
 
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 exports.handler = async function http (req) {
-  let recipes = (await data.get({table: 'recipes'})).map(recipe => recipe.recipe);
+  let recipes = (await data.get({table: 'recipes'})).map(recipe => recipe.recipe.title);
   return {
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
